a465415bd4d6f65c214ab148204b01de4fcb3b1c
eliannahunter <36614956+eliannahunter@users.noreply.github.com>
2018-02-18 23:18:59 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_HunterElianna.py
@@ -0,0 +1,12 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Elianna Hunter
+# 2/16/2018
+
+total_sales = float(input('Enter the projected sales: '))
+
+profit = total_sales * 0.23
+
+print ('The profit is $', format(profit, ',.2f'))
+
+input ("Press enter to exit ")
