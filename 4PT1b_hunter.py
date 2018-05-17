e396e951ba7327e96e4cf62d6b6dc18d4e02b6d7
eliannahunter <36614956+eliannahunter@users.noreply.github.com>
2018-05-16 23:40:14 -0400

    Add files via upload

---
+++ 4PT1b_hunter.py
@@ -0,0 +1,23 @@
+import turtle
+wn = turtle.Screen()
+wn.title("Initials")
+
+e = turtle.Turtle()
+e.shape("turtle")
+e.color("light blue")
+e.pensize(3)
+
+e.forward(100)
+e.backward(100)
+e.left(90)
+e.forward(75)
+e.right(90)
+e.forward(100)
+e.backward(100)
+e.left(90)
+e.forward(75)
+e.right(90)
+e.forward(100)
+e.penup()
+
+
